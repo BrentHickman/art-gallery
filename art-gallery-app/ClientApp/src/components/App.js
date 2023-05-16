e5990171a5d6fe087fc6a9cc387dbd5c9f092dac
BrentHickman <brenthickman@ymail.com>
2023-05-16 12:50:22 -0700

    auth with firebase added.

--- art-gallery-app/ClientApp/src/components/App.js
+++ art-gallery-app/ClientApp/src/components/App.js
@@ -23,12 +23,20 @@
 
 import React from 'react';
 import '../custom.css';
+import SignIn from './SignIn';
 import Gallery from './Gallery';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <React.Fragment>
-      < Gallery />
+      <Router>
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/" element={<Gallery />} />
+        </Routes>
+      </Router>
+
     </React.Fragment>
   );
 }
